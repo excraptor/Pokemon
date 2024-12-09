b85bde95c7eeb067d4f02ee8800027742993427b
Tamas Balla <tamasballa99@gmail.com>
2024-12-09 19:33:16 +0100

    Initial Commit

---
+++ pokemon/pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApp.swift
+//  pokemon
+//
+//  Created by Tamás Balla on 2024. 12. 09..
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
